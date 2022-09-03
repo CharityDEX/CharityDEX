0064ba03688614b74314d637c3b90305e9829d4e
Denis Baturin <ndbaturin@gmail.com>
2022-09-03 17:16:26 +0400

    Updated deployment task

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,9 +7,6 @@ const MNEMONIC = process.env.MNEMONIC;
 const PRIVATE_KEY = process.env.PRIVATE_KEY
 const ACCOUNTS = MNEMONIC ? { "mnemonic": MNEMONIC } : [PRIVATE_KEY];
 
-const GOERLI_URL = process.env.GOERLI_URL || "";
-const ETHEREUM_URL = process.env.ETHEREUM_URL || "";
-
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
@@ -21,7 +18,9 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 });
 
 task("deploy", "Deploys Charity Swap")
-  .addParam("swapRouter", "The Uniswap Swap Router address")
+  .addParam("factoryV2", "The Uniswap V2 factory")
+  .addParam("factoryV3", "The Uniswap V3 factory")
+  .addParam("positionManager", "The Uniswap position manager")
   .addParam("weth", "The WETH contract address")
   .addParam("charity", "The Charity contract address")
   .addOptionalParam("charityFee", "The part of the swap that goes to charity")
@@ -31,7 +30,7 @@ task("deploy", "Deploys Charity Swap")
     const charityFee = taskArgs.charityFee || process.env.CHARITY_FEE || 50000; // defaults to 5%
 
     const CharitySwap = await ethers.getContractFactory("CharitySwap");
-    const charitySwap = await CharitySwap.deploy(taskArgs.swapRouter, taskArgs.weth, taskArgs.charity, charityFee);
+    const charitySwap = await CharitySwap.deploy(taskArgs.factoryV2, taskArgs.factoryV3, taskArgs.positionManager, taskArgs.weth, taskArgs.charity, charityFee);
     await charitySwap.deployTransaction.wait()
 
     console.log("CharitySwap deployed to:", charitySwap.address);
@@ -59,22 +58,12 @@ module.exports = {
   },
   networks: {
     goerli: {
-      url: GOERLI_URL,
+      url: process.env.GOERLI_URL || "none",
       accounts: ACCOUNTS,
     },
     ethereum: {
-      url: ETHEREUM_URL,
+      url: process.env.ETHEREUM_URL || "none",
       accounts: ACCOUNTS,
     },
   },
-  abiExporter: [
-    {
-      path: './abi/pretty',
-      pretty: true,
-    },
-    {
-      path: './abi/ugly',
-      pretty: false,
-    },
-  ]
 };
